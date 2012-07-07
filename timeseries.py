stupid hack for averaged returns, fix it later

timeseries.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-def returns(v, lags, include_original = False, return_weight = 1):
+import pandas
+
+  
+def returns(v, lags, average_future_returns = True, include_original = False, return_weight = 1):
   if not isinstance(lags, list):
     lags = [lags]
   # assuming data is 2d, so turn a 1d vector into an nx1 matrix
@@ -24,13 +27,17 @@ def returns(v, lags, include_original = False, return_weight = 1):
   else: 
     result = np.zeros( (n_short, d * n_lags), dtype='float')
   for i, lag in enumerate(lags):
-    returns = np.log(v[lag:, ] / v[:-lag, :]) 
+    if average_future_returns:
+      averaged = pandas.rolling_mean(v, lag)[lag:]
+      returns = np.log(averaged / v[:-lag, :]) 
+    else:
+      returns = np.log(v[lag:, ] / v[:-lag, :]) 
     # skip beginning of returns series if it's from a lag shorter
     # than max_lag 
     result[:, i*d:(i+1)*d] = returns[(max_lag - lag):, :] * return_weight
   return result
   
-def multiday_returns(v, lags, include_original = False, return_weight = 1, transpose_day = False):
+def multiday_returns(v, lags, average_future_returns = False, include_original = False, include_time = False, return_weight = 1, transpose_day = False):
   assert np.rank(v) == 3
   n_days = v.shape[0]
   result = []
@@ -38,34 +45,61 @@ def multiday_returns(v, lags, include_original = False, return_weight = 1, trans
     day = v[i, :, :]
     if transpose_day:
       day = day.T
-    result.append(returns(day, lags, include_original, return_weight))
+    day_returns = returns(day, lags, average_future_returns, include_original, return_weight)
+    
+    if include_time:
+      n_ticks = day_returns.shape[0]
+      
+      time = np.arange(n_ticks) / float(n_ticks)
+      time_col = np.array([time]).T
+
+      day_returns = np.hstack([ time_col, day_returns])
+    result.append(day_returns)
   return np.vstack(result)
   
-def split_data(v, lags, include_original=False, return_weight=1, transpose_day = False, test_start_day=2):
+def split_data(v, lags, average_future_returns = False, include_original=False, include_time = False, return_weight=1, transpose_day = False, test_start_day=2):
   assert np.rank(v) == 3
   n_days = v.shape[0]
   assert test_start_day < n_days
   train = v[:test_start_day, :, :]
   test = v[test_start_day:, :, :]
-  train_data = multiday_returns(train, lags, include_original, return_weight, transpose_day)
-  test_data = multiday_returns(test, lags, include_original, return_weight, transpose_day)
+  train_data = multiday_returns(train, lags, average_future_returns, include_original, include_time, return_weight, transpose_day)
+  test_data = multiday_returns(test, lags, average_future_returns, include_original, include_time, return_weight, transpose_day)
   return train_data, test_data 
 
-def generate_dataset(v, lags, future_lag, target_idx, include_original=False, return_weight=1, transpose_day = False, test_start_day = 2):
+def generate_dataset(v, lags, future_lag, target_idx = 1, average_future_returns = True, include_original=False, include_time = True, return_weight=10000, transpose_day = True, test_start_day = 11):
   
   train, test = split_data(v, lags, \
     include_original=True, 
+    average_future_returns = False, 
+    include_time = include_time, 
     return_weight=return_weight, 
     transpose_day = transpose_day, 
     test_start_day = test_start_day) 
-  print train.shape[1]
-  print len(lags)
-  n_original_series = train.shape[1] / (len(lags)+1)
-  print n_original_series
-  train_target = train[:, -(n_original_series - target_idx)]
-  test_target = test[:, -(n_original_series - target_idx)]
+
+  n_cols = train.shape[1]
+  n_original_series = (n_cols - (1 if include_time else 0)) / (len(lags)+1)
+  
+  
+  # TODO: This is terrible, fix it! 
+  if average_future_returns:
+    train2, test2 = split_data(v, lags, \
+      include_original=True, 
+      average_future_returns = True, 
+      include_time = include_time, 
+      return_weight=return_weight, 
+      transpose_day = transpose_day, 
+      test_start_day = test_start_day) 
+      
+    train_target = train2[:, -(n_original_series - target_idx)]
+    test_target = test2[:, -(n_original_series - target_idx)]
+  else:
+    train_target = train[:, -(n_original_series - target_idx)]
+    test_target = test[:, -(n_original_series - target_idx)]
+    
   ytrain_return = np.log(train_target[future_lag:] / train_target[:-future_lag])
   ytest_return = np.log(test_target[future_lag:] / test_target[:-future_lag])
+  
   if not include_original:
     train = train[:, :-n_original_series]
     test = test[:, :-n_original_series]
@@ -74,3 +108,48 @@ def generate_dataset(v, lags, future_lag, target_idx, include_original=False, re
   test = test[:-future_lag, :]
   return train, ytrain_return, test, ytest_return
   
+
+from regularized import RegularizedGaussianNB
+from sklearn.linear_model import SGDClassifier
+
+# search for best triplet of lags and best target_idx   
+def search_for_best_dataset_params(v, possible_lags,  future_lags, \
+    target_indices = [0], 
+    return_weight = 10000, 
+    transpose_day = True, 
+    test_start_day = 2, 
+    n_iter = 2):
+  best_acc = 0
+  best_params = None
+  best_model = None
+  target_index_accs = dict( [ (idx, []) for idx in target_indices])
+  for l1 in possible_lags:
+    for l2 in [l for l in possible_lags if l > l1]:
+      for l3 in [l for l in possible_lags if l > l2]:
+        lags = [l1, l2, l3]
+        for target_idx in target_indices:
+          for future_lag in [l for l in future_lags if l <= max(lags)]:
+            print "Generating data for lags = %s, target_idx = %s, future_lag = %s" % (lags, target_idx, future_lag)
+            
+            xtrain, ytrain, xtest, ytest = generate_dataset(v, lags, \
+              future_lag, target_idx, include_original = False, \
+              return_weight = return_weight, 
+              transpose_day = transpose_day, 
+              test_start_day = test_start_day)
+            
+            lr = SGDClassifier(loss = 'log', n_iter = n_iter)
+            print "Training classifier..." 
+            lr.fit(xtrain, np.sign(ytrain))
+            acc = lr.score(xtest, np.sign(ytest))
+            target_index_accs[target_idx].append(acc)
+            print "Accuracy", acc 
+            if acc > best_acc:
+              best_acc = acc
+              best_params = {'lags': lags, 'target_idx': target_idx, 'future_lag': future_lag}
+              best_model = lr 
+  print "Best accuracy: %s, best_params: %s" % (best_acc, best_params)
+  def stats(elts):
+    return {'min': min(elts), 'max': max(elts), 'median':np.median(elts), 'mean':np.mean(elts), 'std': np.std(elts)}
+  target_index_stats = dict([ (idx, stats(elts)) for (idx,elts) in target_index_accs.items()])
+  print target_index_stats 
+  return best_acc, best_params, best_model, target_index_stats

timing.py
@@ -70,7 +70,7 @@ def mk_classifiers(n_samples):
       classifiers['Gradient Boosting (sample size = 25%%, %d stumps)' % n_trees] = mk_boost(0.25, 3, n_trees)
 
   
-  for u in [2 * 10**6, 10**6, 500000, 250000]:
+  for u in [2 * 10**6, 10**6, 250000]:
     n_iter = int(np.ceil(u / float(n_samples)))
     classifiers['Linear SVM (SGD, %d updates / %d epochs)' % (u, n_iter)] = SGDClassifier(loss="hinge", n_iter = n_iter, shuffle=True)
     classifiers['Logisic Regression (SGD, %d updates / %d epochs)' % (u, n_iter)] = SGDClassifier(loss="log", n_iter = n_iter, shuffle = True)    
@@ -150,7 +150,7 @@ def get_timings(X,Y, Xtest = None, Ytest = None):
       train_time = t1 - t0
       test_time = t2 - t1
       result = Result(train_time, test_time, train_time + test_time, train_accuracy, test_accuracy, precision, recall)
-    except np.linalg.LinAlgError:
+    except (np.linalg.LinAlgError, AttributeError, MemoryError, OverflowError, ZeroDivisionError, AssertionError):
        print "Failed: ", sys.exc_info()
     results[name] = result 
     print result 